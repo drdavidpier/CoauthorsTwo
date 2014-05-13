e194e9a1f810856cf0ebb82851797eeb4c9163cb
David Pier <drdavidpier@gmail.com>
2014-05-13 16:09:06 +0100

    search added

---
+++ app/application/views/search.php
@@ -0,0 +1,5 @@
+<form method="post" action="<?php echo base_url('index.php/search/dosearch'); ?>" accept-charset="utf-8">
+	<p><label for="search_term">Search term: </label> 
+	<input type="text" name="search_term" value="some text" id="search_term"> 
+	<input type="submit" value="Search PubMed"></p>
+</form>
